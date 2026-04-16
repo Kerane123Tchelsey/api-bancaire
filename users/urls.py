07dd9e57f2a687e210d1cf9cb587a1daf4acec55
Kerane123Tchelsey <keranetonlio1@gmail.com>
2026-04-16 17:33:15 +0100

    Correction URLs utilisateurs

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Liste des utilisateurs + création
     path('api/utilisateurs/', views.UtilisateurListCreateView.as_view(), name='liste_utilisateurs'),
-    
-    # Détail d'un utilisateur spécifique
     path('api/utilisateurs/<int:pk>/', views.UtilisateurDetailView.as_view(), name='detail_utilisateur'),
 ]
