f2479810a59e00963e49b7daf531854144003c39
Kerane123Tchelsey <keranetonlio1@gmail.com>
2026-04-27 20:40:23 +0100

    Fix url configurations

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/utilisateurs/', views.UtilisateurListCreateView.as_view(), name='liste_utilisateurs'),
-    path('api/utilisateurs/<int:pk>/', views.UtilisateurDetailView.as_view(), name='detail_utilisateur'),
+    path('utilisateurs/', views.UtilisateurListCreateView.as_view(), name='liste_utilisateurs'),
+    path('utilisateurs/<int:pk>/', views.UtilisateurDetailView.as_view(), name='detail_utilisateur'),
 ]
