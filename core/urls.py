214344b87cbcf2f11c61fcbb6a86384136e41ba7
Kerane123Tchelsey <keranetonlio1@gmail.com>
2026-04-29 16:14:28 +0100

    Fix swagger URL

--- core/urls.py
+++ core/urls.py
@@ -8,7 +8,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="API Bancaire",
         default_version='v1',
-        description="API pour gérer les utilisateurs d'un système bancaire",
+        description="API pour gérer les utilisateurs",
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
@@ -17,6 +17,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
 ]
