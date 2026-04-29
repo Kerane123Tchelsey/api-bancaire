2ef16eb75b3884c3e9e0a9da470a86d5015db47f
Kerane123Tchelsey <keranetonlio1@gmail.com>
2026-04-29 17:29:56 +0100

    Revert to working swagger.json state

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -21,4 +19,4 @@ urlpatterns = [
     path('api/', include('users.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
