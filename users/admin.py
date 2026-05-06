257152acec100be88da49dbe40fa3cd0cffb8fea
Kerane123Tchelsey <keranetonlio1@gmail.com>
2026-05-06 15:31:33 +0100

    Correction: suppression de l'import Licence

--- users/admin.py
+++ users/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
-from .models import UtilisateurBancaire, Licence
+from .models import UtilisateurBancaire, Banque, Transaction
 
 @admin.register(UtilisateurBancaire)
 class UtilisateurBancaireAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nom', 'prenom', 'email', 'numero_compte', 'solde', 'statut')
-    list_filter = ('statut',)
+    list_display = ('nom', 'prenom', 'email', 'numero_compte', 'solde', 'banque', 'is_admin', 'statut')
+    list_filter = ('statut', 'is_admin', 'banque')
     search_fields = ('nom', 'prenom', 'email', 'numero_compte')
-    readonly_fields = ('id', 'date_creation', 'date_modification')
 
-@admin.register(Licence)
-class LicenceAdmin(admin.ModelAdmin):
-    list_display = ('utilisateur', 'type_licence', 'statut', 'date_debut')
-    list_filter = ('type_licence', 'statut')
-    search_fields = ('utilisateur__email',)
+@admin.register(Banque)
+class BanqueAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'code_swift', 'adresse')
+    search_fields = ('nom', 'code_swift')
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('destinataire', 'montant', 'type_transaction', 'statut', 'date_creation')
+    list_filter = ('type_transaction', 'statut')
+    search_fields = ('destinataire__nom', 'expediteur__nom')
